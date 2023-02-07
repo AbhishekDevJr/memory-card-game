1581b8d21d03ac27ba69a3e8909d3d3cd82a7645
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-07 18:25:02 +0530

    MainSection component with media queries in progress

---
+++ memory-card-game/src/components/MainSection.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+const MainSection = () => {
+    return(
+        <div className = "main-sec">
+            <div className = "main-container">
+                <div className = "row1">
+                    <div className = "card1">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card2">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card3">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card4">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+                </div>
+
+                <div className = "row2">
+                    <div className = "card5">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card6">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card7">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card8">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+                </div>
+
+                <div className = "row3">
+                    <div className = "card9">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card10">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card11">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+
+                    <div className = "card12">
+                        <img src = "" alt = "Pokemon Img" />
+                        <p>{'Placeholder'}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainSection;
