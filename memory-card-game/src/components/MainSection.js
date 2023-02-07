f31fdb88ed07f84b08805454460bb41462c573d9
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-07 22:58:29 +0530

    Randomizing Images on user action logic in progress

--- memory-card-game/src/components/MainSection.js
+++ memory-card-game/src/components/MainSection.js
@@ -2,25 +2,31 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 
 const MainSection = () => {
-    const [imgSrc, setImgSrc] = useState({
-        poke1 : ['poke1.png', 'Charmeleon'],
-        poke2 : ['poke2.png', 'Bulbasaur'],
-        poke3 : ['poke3.1.png', 'Venusaur'],
-        poke4 : ['poke4.png', 'Squirtle'],
-        poke5 : ['poke5.png','Ivysaur'],
-        poke6 : ['poke6.png','Wartortle'],
-        poke7 : ['poke7.png','Charmander'],
-        poke8 : ['poke8.png','Blastoise'],
-        poke9 : ['poke9.png','Charizard'],
-        poke10 : ['poke10.png','Caterpie'],
-        poke11 : ['poke11.png','Metapod'],
-        poke12 : ['poke12.png','Butterfree'],
-    });
+    const [imgSrc, setImgSrc] = useState([
+        {poke1 : ['poke1.png', 'Charmeleon']},
+        {poke2 : ['poke2.png', 'Bulbasaur']},
+        {poke3 : ['poke3.1.png', 'Venusaur']},
+        {poke4 : ['poke4.png', 'Squirtle']},
+        {poke5 : ['poke5.png','Ivysaur']},
+        {poke6 : ['poke6.png','Wartortle']},
+        {poke7 : ['poke7.png','Charmander']},
+        {poke8 : ['poke8.png','Blastoise']},
+        {poke9 : ['poke9.png','Charizard']},
+        {poke10 : ['poke10.png','Caterpie']},
+        {poke11 : ['poke11.png','Metapod']},
+        {poke12 : ['poke12.png','Butterfree']}
+    ]);
 
     const cardHandler = (e) => {
-        setImgSrc({
-            poke1 : ['poke1.png', 'Charmander']
+        imgSrc.forEach((pokeObj, index, arr) => {
+
         });
+
+        setImgSrc(
+            imgSrc.forEach((pokeObj, index, arr) => {
+
+            }) 
+        );
     }
 
     return(
@@ -28,67 +34,67 @@ const MainSection = () => {
             <div className = "main-container">
                 <div className = "row1">
                     <div onClick = {cardHandler} className = "card1">
-                        <img src = {imgSrc.poke1[0]} alt = "Pokemon Img" />
-                        <p>{imgSrc.poke1[1]}</p>
+                        <img src = {imgSrc[0].poke1[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[0].poke1[1]}</p>
                     </div>
 
-                    <div className = "card2">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card2">
+                        <img src = {imgSrc[1].poke2[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[1].poke2[1]}</p>
                     </div>
 
-                    <div className = "card3">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card3">
+                        <img src = {imgSrc[2].poke3[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[2].poke3[1]}</p>
                     </div>
 
-                    <div className = "card4">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card4">
+                        <img src = {imgSrc[3].poke4[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[3].poke4[1]}</p>
                     </div>
                 </div>
 
                 <div className = "row2">
-                    <div className = "card5">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card5">
+                        <img src = {imgSrc[4].poke5[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[4].poke5[1]}</p>
                     </div>
 
-                    <div className = "card6">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card6">
+                        <img src = {imgSrc[5].poke6[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[5].poke6[1]}</p>
                     </div>
 
-                    <div className = "card7">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card7">
+                        <img src = {imgSrc[6].poke7[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[6].poke7[1]}</p>
                     </div>
 
-                    <div className = "card8">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card8">
+                        <img src = {imgSrc[7].poke8[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[7].poke8[1]}</p>
                     </div>
                 </div>
 
                 <div className = "row3">
-                    <div className = "card9">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card9">
+                        <img src = {imgSrc[8].poke9[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[8].poke9[1]}</p>
                     </div>
 
-                    <div className = "card10">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card10">
+                        <img src = {imgSrc[9].poke10[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[9].poke10[1]}</p>
                     </div>
 
-                    <div className = "card11">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card11">
+                        <img src = {imgSrc[10].poke11[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[10].poke11[1]}</p>
                     </div>
 
-                    <div className = "card12">
-                        <img src = "" alt = "Pokemon Img" />
-                        <p>{'Placeholder'}</p>
+                    <div onClick = {cardHandler} className = "card12">
+                        <img src = {imgSrc[11].poke12[0]} alt = "Pokemon Img" />
+                        <p>{imgSrc[11].poke12[1]}</p>
                     </div>
                 </div>
             </div>
